9a46c7cef6f6da91520e3c7b65881f61adbd8b58
Johan Eidmann <johan.eidmann1995@hotmail.com>
2021-01-10 17:11:39 +0100

    seanste

--- src/main/java/api/GetResponse.java
+++ src/main/java/api/GetResponse.java
@@ -1,13 +1,19 @@
 package main.java.api;
 
-import java.util.ArrayList; 
+import java.net.URI;
+import java.util.ArrayList;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+//import org.glassfish.jersey.media.multipart.FormDataParam;
 
 @Path("response")
 public class GetResponse {
@@ -44,6 +50,44 @@ public class GetResponse {
 		ArrayList<getCourse> App = new App().searchLektInfo(courseId,startDate,endDate, lektId);
 		App.toString();
 		return Response.ok(App).build();
+	}
+	
+	@POST
+	@Path("/postEvent/{title}/{start}/{end}/{date}/{descr}/{loc}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response postEvent(@PathParam("title")String title,@PathParam("start")String start,
+			@PathParam("end")String end,@PathParam("date")String date,@PathParam("descr")String descr,@PathParam("loc")String loc) throws Exception {
+		System.out.println("test");
+		test3 test = new test3();
+		
+//		ArrayList<String> test3 = new ArrayList();
+//		test3.set(9, titel);
+		
+		
+				
+		test.sendCanvas(title,start,end,date,descr,loc);
+		
+		return Response.ok().build();
+		
+	}
+	
+	@POST
+	@Path("/postCanvas")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response postEventForm(@FormParam("title")String title,@FormParam("start")String start,
+			@FormParam("end")String end,@FormParam("date")String date,@FormParam("descr")String descr,@FormParam("loc")String loc) throws Exception {
+		System.out.println("test");
+		test3 test = new test3();
+		
+//		ArrayList<String> test3 = new ArrayList();
+//		test3.set(9, titel);
+		
+		
+				
+		test.sendCanvas(title,start,end,date,descr,loc);
+		
+		return Response.created(URI.create("response/" + test)).build();
+		
 	}
 	
 	
